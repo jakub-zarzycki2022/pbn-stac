e5817b211f1ceac2c6a405ae65bd20b4b02ca35b
Jakub Zarzycki <jakub.zarzycki@ideas-ncbr.pl>
2025-09-15 14:16:34 +0200

    run comp updated

--- run_comp.sh
+++ run_comp.sh
@@ -1,7 +1,7 @@
 for i in *.bnet; do
     echo "$i"
-    # run cabean with 1 minute timeout
-    time timeout 3m cabean -compositional 2 "$i"
+    # run cabean with 1 minute timeouthttps://allegro.pl/oferta/glosniki-komputerowe-2-0-usb-28w-pc-laptop-14281268771
+    (time timeout 30m cabean -compositional 2 "$i") >> out.log2 2>&1
     # always run python step after (regardless of timeout)
-    time python detect_attractors_test.py -f "$i" --type bnet
+    (time python detect_attractors_test.py -f "$i" --type bnet) >> out.log2 2>&1
 done
